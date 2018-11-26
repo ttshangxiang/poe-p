d332724faca39280c2e13a2efde6de42ecade19a
jianghuaqing <jianghuaqing@haizhi.com>
2018-11-26 18:56:31 +0800

    0

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var log = require('fancy-log');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+
+// 在这里添加自定义 browserify 选项
+var customOpts = {
+  entries: ['./src/app.ts'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+// 在这里加入变换操作
+// 比如： b.transform(coffeeify);
+
+gulp.task('dev', ['html'], bundle); // 这样你就可以运行 `gulp js` 来编译文件了
+b.on('update', bundle); // 当任何依赖发生改变的时候，运行打包工具
+b.on('log', log); // 输出编译日志到终端
+
+function bundle() {
+  return b.plugin('tsify', {
+      noImplicitAny: true
+    })
+    .bundle()
+    // 如果有错误发生，记录这些错误
+    .on('error', err => log.error(err))
+    .pipe(source('bundle.js'))
+    // 可选项，如果你不需要缓存文件内容，就删除
+    .pipe(buffer())
+    // 可选项，如果你不需要 sourcemaps，就删除
+    // .pipe(sourcemaps.init({
+    //   loadMaps: true
+    // })) 
+    // 从 browserify 文件载入 map
+    // 在这里将变换操作加入管道
+    // .pipe(sourcemaps.write('./')) // 写入 .map 文件
+    .pipe(gulp.dest('./dist'));
+}
+
+gulp.task('html', function () {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+})
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+
+import t from "./t"
+
+alert(t)
